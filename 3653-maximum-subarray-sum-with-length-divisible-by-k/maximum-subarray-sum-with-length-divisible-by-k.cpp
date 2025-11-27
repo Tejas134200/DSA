99f995431c12b260b63e44323e5ec7daabfe8e30
Tejas Pokalwar <pokalwartejas30102005@gmail.com>
2025-11-27 18:24:45 +0530

    Time: 2 ms (95.22%) | Memory: 162.2 MB (90.10%) - LeetSync

---
+++ 3653-maximum-subarray-sum-with-length-divisible-by-k/maximum-subarray-sum-with-length-divisible-by-k.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    long long maxSubarraySum(vector<int>& nums, int k) {
+        int n = nums.size();
+        long long prefixSum =0;
+        long long mSum = LONG_LONG_MIN;
+        vector<long long> kSum(k,LONG_LONG_MAX/2);
+        kSum[k-1]=0;
+        for(int i =0;i<nums.size();i++){
+            prefixSum+=nums[i];
+            mSum = max(mSum,prefixSum-kSum[i%k]);
+            kSum[i%k]= min(kSum[i%k],prefixSum);
+        }
+        return mSum;
+
+        
+    }
+};
