4128b3aa5de64ce3f545103c713f316347364c6f
Tejas Pokalwar <pokalwartejas30102005@gmail.com>
2025-12-03 09:28:29 +0530

    Time: 217 ms (28.52%) | Memory: 129.6 MB (61.64%) - LeetSync

--- 2531-create-components-with-same-value/create-components-with-same-value.cpp
+++ 2531-create-components-with-same-value/create-components-with-same-value.cpp
@@ -17,12 +17,13 @@ public:
     }
 
     int dfsSum(int cur , int par  ,int& comp , int tar ){
-        int sum = nums[cur];
+        int sum =0;
         for(auto& it:adj[cur]){
             if(it!=par){
                 sum+=dfsSum(it,cur,comp,tar);
             }
         }
+        sum += nums[cur];
         if(sum==tar){
             comp++;
             return 0;
@@ -42,16 +43,20 @@ public:
             adj[e[1]].push_back(e[0]);
         }
 
-        dfs(0,-1);
+        // dfs(0,-1);
 
         
 
         int best = 0;
-        for (int target : sums) {
+        for (int target =1;target<=total;target++) {
+            if(total%target==0){
             int comp = 0;
             dfsSum(0, -1, comp, target);
             if (comp == total / target)
                 best = max(best, comp);
+            }
+            
+            
         }
 
         return best - 1;
