61498e0bd05d9afd1af6291c7dde613a0105e0e9
Tejas Pokalwar <pokalwartejas30102005@gmail.com>
2025-12-03 09:20:03 +0530

    Time: 111 ms (80.33%) | Memory: 129.8 MB (61.31%) - LeetSync

---
+++ 2531-create-components-with-same-value/create-components-with-same-value.cpp
@@ -0,0 +1,64 @@
+class Solution {
+public:
+    set<int> sums;
+    vector<vector<int>> adj;
+    vector<int> nums;
+    int total;
+
+     int dfs(int cur, int par) {
+        int sum = nums[cur];
+        for (int next : adj[cur]) {
+            if (next != par)
+                sum += dfs(next, cur);
+        }
+        if (total % sum == 0) 
+            sums.insert(sum);
+        return sum;
+    }
+
+    int dfsSum(int cur , int par  ,int& comp , int tar ){
+        int sum = nums[cur];
+        for(auto& it:adj[cur]){
+            if(it!=par){
+                sum+=dfsSum(it,cur,comp,tar);
+            }
+        }
+        if(sum==tar){
+            comp++;
+            return 0;
+        }
+        return sum;
+    }
+
+    int componentValue(vector<int>& nums, vector<vector<int>>& edges) {
+        this->nums = nums;
+        total =0;
+        for(int i:nums) total+=i;
+        int n = nums.size();
+        adj.assign(n, {});
+
+        for (auto &e : edges) {
+            adj[e[0]].push_back(e[1]);
+            adj[e[1]].push_back(e[0]);
+        }
+
+        dfs(0,-1);
+
+        
+
+        int best = 0;
+        for (int target : sums) {
+            int comp = 0;
+            dfsSum(0, -1, comp, target);
+            if (comp == total / target)
+                best = max(best, comp);
+        }
+
+        return best - 1;
+
+
+
+
+        
+    }
+};
