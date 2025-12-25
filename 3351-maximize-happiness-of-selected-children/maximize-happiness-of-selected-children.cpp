6149dc319a2782895ff2ef307af82df6ff26fa74
Tejas Pokalwar <pokalwartejas30102005@gmail.com>
2025-12-25 09:12:38 +0530

    Time: 232 ms (5.67%) | Memory: 110.2 MB (25.18%) - LeetSync

--- 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.cpp
+++ 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.cpp
@@ -3,14 +3,8 @@ public:
     long long maximumHappinessSum(vector<int>& happiness, int k) {
         sort(happiness.rbegin(),happiness.rend());
         long long sum =happiness[0];
-        int dec=1;
-
+        for(int i=1;i<k;i++)    sum+=max(0,happiness[i]-i);   
         
-        k--;
-        for(int i=1;i<=k;i++){
-            sum+=max(0,happiness[i]-dec);
-            dec++;
-        }
        return sum;
 
 
