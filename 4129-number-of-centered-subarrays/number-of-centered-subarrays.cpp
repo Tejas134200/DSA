508318ea10fe3409ad4eec41bd3726368d3e96a3
Tejas Pokalwar <pokalwartejas30102005@gmail.com>
2026-01-12 10:33:20 +0530

    Time: 611 ms (61.54%) | Memory: 245.8 MB (61.54%) - LeetSync

---
+++ 4129-number-of-centered-subarrays/number-of-centered-subarrays.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int centeredSubarrays(vector<int>& nums) {
+        
+        int n = nums.size();
+        int ans =0;
+        for(int i =0;i<n;i++){
+            unordered_map<int,int> m;
+            int sum =0;
+            for(int j =i;j<n;j++){
+                sum+=nums[j];
+                m[nums[j]]++;
+                if(m[sum]!=0) ans++;
+                
+            }
+        }
+        return ans;
+    }
+};
