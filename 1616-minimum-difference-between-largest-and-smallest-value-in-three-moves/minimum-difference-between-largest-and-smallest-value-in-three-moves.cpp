64f9e37cd9cacf1469f54d0a49a6c1c48fa1b07a
Tejas Pokalwar <pokalwartejas30102005@gmail.com>
2026-01-26 10:52:51 +0530

    Time: 16 ms (58.54%) | Memory: 42.4 MB (51.52%) - LeetSync

--- 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
+++ 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
@@ -5,9 +5,9 @@ public:
         if(n<=4) return 0;
         sort(nums.begin(),nums.end());
         int mini = INT_MAX;
-        int rem = n-3;
-        for(int i =0 ; i+rem-1<n;i++){
-            mini = min(mini , nums[i+rem-1]-nums[i]);
+        int rem = n-4;
+        for(int i =0 ; i+rem<n;i++){
+            mini = min(mini , nums[i+rem]-nums[i]);
         }
         return mini;
     }
