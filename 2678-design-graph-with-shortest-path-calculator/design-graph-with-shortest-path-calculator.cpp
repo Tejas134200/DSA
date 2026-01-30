77a28298a2d53a3d11d3522cd6bedb1e79b6ff78
Tejas Pokalwar <pokalwartejas30102005@gmail.com>
2026-01-30 18:12:13 +0530

    Time: 118 ms (32.41%) | Memory: 69.7 MB (82.31%) - LeetSync

--- 2678-design-graph-with-shortest-path-calculator/design-graph-with-shortest-path-calculator.cpp
+++ 2678-design-graph-with-shortest-path-calculator/design-graph-with-shortest-path-calculator.cpp
@@ -2,44 +2,46 @@ class Graph {
 public:
     vector<vector<long long>> adj;
     int n;
+    const long long INF = 1e18;
+
     Graph(int n, vector<vector<int>>& edges) {
         this->n = n;
-        adj.resize(n,vector<long long>(n,INT_MAX));
-        for(int i =0;i<n;i++) adj[i][i]=0;
-        for(auto v:edges){
-            adj[v[0]][v[1]]=v[2];
+        adj.assign(n, vector<long long>(n, INF));
+
+        for(int i = 0; i < n; i++) adj[i][i] = 0;
+
+        for(auto &v : edges){
+            int u = v[0], w = v[2], to = v[1];
+            adj[u][to] = min(adj[u][to], (long long)w);
         }
-        for(int k = 0;k<n;k++){
-            for(int i =0;i<n;i++){
-                for(int j =0;j<n;j++){
-                    adj[i][j] = min(adj[i][j],adj[i][k]+adj[k][j]);
+
+        for(int k = 0; k < n; k++){
+            for(int i = 0; i < n; i++){
+                if(adj[i][k] == INF) continue;
+                for(int j = 0; j < n; j++){
+                    if(adj[k][j] == INF) continue;
+                    adj[i][j] = min(adj[i][j], adj[i][k] + adj[k][j]);
                 }
             }
         }
     }
-    
+
     void addEdge(vector<int> edge) {
-        int u = edge[0];
-        int v = edge[1];
-        int w = edge[2];
+        int u = edge[0], v = edge[1], w = edge[2];
+
+        if(w >= adj[u][v]) return;
         adj[u][v] = w;
-        for(int k = 0;k<n;k++){
-            for(int i =0;i<n;i++){
-                for(int j =0;j<n;j++){
-                    adj[i][j] = min(adj[i][j],adj[i][k]+adj[k][j]);
-                }
+
+        for(int i = 0; i < n; i++){
+            if(adj[i][u] == INF) continue;
+            for(int j = 0; j < n; j++){
+                if(adj[v][j] == INF) continue;
+                adj[i][j] = min(adj[i][j], adj[i][u] + w + adj[v][j]);
             }
         }
     }
-    
+
     int shortestPath(int node1, int node2) {
-        return adj[node1][node2]==INT_MAX?-1:adj[node1][node2];
+        return adj[node1][node2] == INF ? -1 : (int)adj[node1][node2];
     }
 };
-
-/**
- * Your Graph object will be instantiated and called as such:
- * Graph* obj = new Graph(n, edges);
- * obj->addEdge(edge);
- * int param_2 = obj->shortestPath(node1,node2);
- */
