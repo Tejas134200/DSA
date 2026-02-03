dc93e1f661810bce9adefeeb808526a050eb050c
Tejas Pokalwar <pokalwartejas30102005@gmail.com>
2026-02-03 17:33:03 +0530

    Time: 319 ms (13.82%) | Memory: 211.9 MB (8.81%) - LeetSync

--- 2439-longest-cycle-in-a-graph/longest-cycle-in-a-graph.cpp
+++ 2439-longest-cycle-in-a-graph/longest-cycle-in-a-graph.cpp
@@ -1,41 +1,34 @@
 class Solution {
 public:
-    int ans = -1;
 
-    void dfs(int in,
-             unordered_map<int,int>& mp,
-             vector<int>& edges,
-             vector<int>& vis) {
-
-        vis[in] = 1;
+    void dfs(int in , unordered_map<int,int>& mp ,vector<int>& edges,vector<int>& vis){
+        vis[in] =1;
         int neigh = edges[in];
-
-        if (neigh == -1) return;
-
-        // already processed globally → stop
-        if (vis[neigh] && mp.find(neigh) == mp.end()) return;
-
-        // new node in current path
-        if (mp.find(neigh) == mp.end()) {
-            mp[neigh] = mp[in] + 1;
-            dfs(neigh, mp, edges, vis);
+        if(neigh==-1) return;
+        if(vis[neigh]==0&&mp.find(neigh)==mp.end()){
+            mp[neigh] = 1+mp[in];
+            dfs(neigh , mp , edges , vis);
         }
-        // cycle detected
-        else {
-            ans = max(ans, mp[in] - mp[neigh] + 1);
+        else if(vis[neigh]==1&&mp.count(neigh)){
+            int k = mp[in]-mp[neigh]+1;
+            ans=max(ans , k);
         }
+        return;
     }
+    int ans;
 
     int longestCycle(vector<int>& edges) {
         int n = edges.size();
-        vector<int> vis(n, 0);
-
-        for (int i = 0; i < n; i++) {
-            if (!vis[i]) {
+        vector<int> dp(n,-1);
+        vector<int> vis(n,0);
+        int maxi =-1;
+        int c =0;
+        ans =-1;
+        for(int i =0;i<n;i++){
+            if(vis[i]==0){
                 unordered_map<int,int> mp;
-                mp.reserve(64);            // small but important
-                mp[i] = 1;
-                dfs(i, mp, edges, vis);
+                mp[i]=1;
+                dfs(i,mp,edges,vis);
             }
         }
         return ans;
