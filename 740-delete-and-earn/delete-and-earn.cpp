63cee0e5d632937a6b14909271ca557744599ea1
Tejas Pokalwar <pokalwartejas30102005@gmail.com>
2026-02-28 16:45:44 +0530

    Time: 3 ms (58.79%) | Memory: 20.5 MB (9.79%) - LeetSync

---
+++ 740-delete-and-earn/delete-and-earn.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int deleteAndEarn(vector<int>& nums) {
+        vector<int> freq(10001,0);
+        for(int i:nums) freq[i]++;
+        vector<int> dp(10001,-1);
+        dp[0]=0;
+        dp[1]=freq[1];
+        for(int i=2;i<10001;i++){
+            dp[i] = max(dp[i-2]+i*freq[i],dp[i-1]);
+        }
+        return dp[10000];
+
+        
+    }
+};
