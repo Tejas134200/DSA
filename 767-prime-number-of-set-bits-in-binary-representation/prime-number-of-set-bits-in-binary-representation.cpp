2abe22aa6c2ae6fc53aa62581744cf6a5c816ef9
Tejas Pokalwar <pokalwartejas30102005@gmail.com>
2026-03-03 19:57:13 +0530

    Time: 4 ms (47.65%) | Memory: 8 MB (66.74%) - LeetSync

---
+++ 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isp(int x){
+        if(x==1) return false;
+        for(int i=2;i*i<=x;i++){
+            if(x%i==0) return false;
+        }
+        return true;
+    }
+    int countPrimeSetBits(int left, int right) {
+        int ans =0;
+        for(int i = left;i<=right;i++){
+            if(isp(__builtin_popcount(i))) ans++;
+        }
+        return ans;
+    }
+};
