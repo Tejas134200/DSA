391797d337a0081f763aec3319653e39c348bf4f
Tejas Pokalwar <pokalwartejas30102005@gmail.com>
2026-03-05 17:18:51 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (76.20%) - LeetSync

--- 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.cpp
+++ 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.cpp
@@ -2,13 +2,7 @@ class Solution {
 public:
     int minOperations(string s) {
         int c1 =0,c2=0;
-        for(int i =0;i<s.size();i++){
-            if(!(i%2)){
-                s[i]=='1'?c1++:c2++;                
-            }else{
-                s[i]=='1'?c2++:c1++;
-            }
-        }
+        for(int i =0;i<s.size();i++) i%2?(s[i]=='1'?c2++:c1++):(s[i]=='1'?c1++:c2++); 
         return min(c1 , c2);
     }
 };
