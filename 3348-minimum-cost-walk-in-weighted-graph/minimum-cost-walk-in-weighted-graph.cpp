2c5bb2481c1fe344ddade10deff97e8b534c9dde
Tejas Pokalwar <pokalwartejas30102005@gmail.com>
2026-03-12 15:17:12 +0530

    Time: 22 ms (58.08%) | Memory: 238.8 MB (90.72%) - LeetSync

---
+++ 3348-minimum-cost-walk-in-weighted-graph/minimum-cost-walk-in-weighted-graph.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    vector<int> par, r;
+
+    int find(int a){
+        if(par[a] != a) par[a] = find(par[a]);
+        return par[a];
+    }
+
+    void unionf(int a, int b){
+        int aa = find(a);
+        int bb = find(b);
+
+        if(aa == bb) return;
+
+        if(r[aa] < r[bb]) par[aa] = bb;
+        else if(r[bb] < r[aa]) par[bb] = aa;
+        else{
+            par[bb] = aa;
+            r[aa]++;
+        }
+    }
+
+    vector<int> minimumCost(int n, vector<vector<int>>& edges, vector<vector<int>>& query) {
+        par.resize(n);
+        r.resize(n,0);
+
+        for(int i=0;i<n;i++) par[i] = i;
+
+        vector<int> ans;
+
+        for(auto &i:edges) unionf(i[0], i[1]);
+
+        unordered_map<int,int> mp;
+
+        for(auto &i:edges){
+            int p = find(i[0]);
+            if(mp.find(p) == mp.end()) mp[p] = i[2];
+            else mp[p] &= i[2];
+        }
+
+        for(auto &i:query){
+            int p = find(i[0]);
+            int q = find(i[1]);
+            if(p != q) ans.push_back(-1);
+            else ans.push_back(mp[p]);
+        }
+
+        return ans;
+    }
+};
