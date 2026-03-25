70e2d43497dd44ac61ea55aa0e9c7a06e42243c2
Tejas Pokalwar <pokalwartejas30102005@gmail.com>
2026-03-25 16:35:03 +0530

    Time: 121 ms (31.50%) | Memory: 110.8 MB (81.65%) - LeetSync

--- 575-distribute-candies/distribute-candies.cpp
+++ 575-distribute-candies/distribute-candies.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int distributeCandies(vector<int>& candyType) {
         set<int> s;
-        for(int i:candyType) s.insert(i);
+        for(int i:candyType){ s.insert(i);if(s.size()>candyType.size()/2) return candyType.size()/2;}
 
         return min((int)candyType.size()/2 , (int)s.size());
     }
