d70060c278ac694bf9cd4c90300f62f4d58b0b9b
Tejas Pokalwar <pokalwartejas30102005@gmail.com>
2026-03-26 13:59:31 +0530

    Time: 14 ms (55.07%) | Memory: 43 MB (45.30%) - LeetSync

--- 621-task-scheduler/task-scheduler.cpp
+++ 621-task-scheduler/task-scheduler.cpp
@@ -1,19 +1,37 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        vector<int> freq(26, 0);
+       vector<int> freq(26,0);
+       priority_queue<int> pq;
+       queue<pair<int,int>> q;
+       int time = 0;
+       for(char c:tasks) freq[c-'A']++;
+       for(int i =0;i<26;i++){
 
-        for(char c : tasks) freq[c - 'A']++;
+            if(freq[i]>0)  pq.push(freq[i]);
+           
+       }
+       while(!pq.empty()||!q.empty()){
+            time++;
+            if(!pq.empty()){
+                auto topi = pq.top();
+                pq.pop();
+            
 
-        int maxFreq = *max_element(freq.begin(), freq.end());
+                topi--;
+                if(topi>0)  q.push({topi,time+n});
+            
+            }
 
-        int countMax = 0;
-        for(int f : freq){
-            if(f == maxFreq) countMax++;
-        }
+            while(!q.empty()&&q.front().second<=time){
+                pq.push(q.front().first);
+                q.pop();
+            }
 
-        int slots = (maxFreq - 1) * (n + 1) + countMax;
+       }
 
-        return max((int)tasks.size(), slots);
+       return time;
+
+    
     }
 };
