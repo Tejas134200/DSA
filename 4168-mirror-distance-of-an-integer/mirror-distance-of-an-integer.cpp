134cc9e208492598310d84995823942ab433a7de
Tejas Pokalwar <pokalwartejas30102005@gmail.com>
2026-04-18 10:22:01 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (18.92%) - LeetSync

--- 4168-mirror-distance-of-an-integer/mirror-distance-of-an-integer.cpp
+++ 4168-mirror-distance-of-an-integer/mirror-distance-of-an-integer.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
-    int mirrorDistance(int n) {
+    int rev(int n){
         string s = to_string(n);
         reverse(s.begin(),s.end());
-        int k = stoi(s);
-        return abs(k-n);
-
+        int t = stoi(s);
+        return t;
+    }
+    int mirrorDistance(int n) {
+        return abs(n-rev(n));
+        
     }
 };
