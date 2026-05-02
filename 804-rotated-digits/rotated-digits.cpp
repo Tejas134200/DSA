ceaaf88f90dc6685ee377a78174e0e6c43f69952
Tejas Pokalwar <pokalwartejas30102005@gmail.com>
2026-05-02 11:23:29 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (18.77%) - LeetSync

--- 804-rotated-digits/rotated-digits.cpp
+++ 804-rotated-digits/rotated-digits.cpp
@@ -1,46 +1,31 @@
 class Solution {
 public:
-    unordered_map<int,int> mp;
-
-    bool isGood(int x){
-        int copy = x;
-        //build from right 788
-
-        int cx = 0;
-        int mul = 1;
-
-        while(x>0){
-            int dig = x%10;
-            x/=10;
-
-            int rot = mp[dig];
-            if(rot==-1) return false;
-
-            cx = mul*rot+cx;
-            mul*=10;
-        }
-        return copy!=cx;
-
-    }
     int rotatedDigits(int n) {
-        int ans = 0;
-        mp[6]=9;
-        mp[9]=6;
-        mp[2]=5;
-        mp[5]=2;
-        mp[0]=0;
-        mp[1]=1;
-        mp[8]=8;
-        mp[3]=-1;
-        mp[4]=-1;
-        mp[7]=-1;
+        vector<int> dp(n + 1, 0);
+        int count = 0;
 
-        for(int i = 1;i<=n;i++){
-            if(isGood(i)) {
-                cout<<i<<endl;
-                ans++;
+        for(int i = 0; i <= n; i++){
+            if(i < 10){
+                if(i == 0 || i == 1 || i == 8) dp[i] = 1;
+                else if(i == 2 || i == 5 || i == 6 || i == 9){
+                    dp[i] = 2;
+                    count++;
+                }
+                else dp[i] = 0;
+            }
+            else{
+                int a = dp[i / 10];
+                int b = dp[i % 10];
+
+                if(a == 1 && b == 1) dp[i] = 1;
+                else if(a >= 1 && b >= 1){
+                    dp[i] = 2;
+                    count++;
+                }
+                else dp[i] = 0;
             }
         }
-        return ans;
+
+        return count;
     }
 };
