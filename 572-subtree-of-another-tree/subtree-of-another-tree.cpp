9cfc4d42304fa83ccaae1a03c12cd551ebfb8a6f
Tejas Pokalwar <pokalwartejas30102005@gmail.com>
2026-06-27 10:48:49 +0530

    Time: 0 ms (100.00%) | Memory: 28.8 MB (94.30%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.cpp
+++ 572-subtree-of-another-tree/subtree-of-another-tree.cpp
@@ -6,27 +6,32 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
-         if(root==NULL && subRoot==NULL) return true;
-        if(root==NULL|| subRoot==NULL) return false;
-        if(root->val==subRoot->val){
-            if(isIdentical(root,subRoot)) return true;
+    bool isIdentical(TreeNode* root, TreeNode* subRoot) {
+        if (!root && !subRoot) {
+            return true;
         }
-        int isLeftSubtree=isSubtree(root->left,subRoot);
-        if(!isLeftSubtree){
-            return isSubtree(root->right,subRoot);
+        if(!root||!subRoot){
+            return false;
         }
-        return true;
+        if (root->val != subRoot->val) {
+            return false;
+        }
+        return (isIdentical(root->left, subRoot->left) &&
+                isIdentical(root->right, subRoot->right));
     }
-    bool isIdentical(TreeNode* p, TreeNode* q) {
-        if(p==NULL && q==NULL) return true;
-        if(p==NULL|| q==NULL) return false;
-        if(p->val!=q->val) return false;
-        return isIdentical(p->left,q->left) && isIdentical(p->right,q->right);
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        if (root == NULL && subRoot) {
+            return false;
+        }
+        if (isIdentical(root, subRoot)) {
+            return true;
+        }
+        return isSubtree(root->left, subRoot)||isSubtree(root->right, subRoot);
     }
 };
