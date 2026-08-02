93b3273597a5d31c43d20e24607b054d671c6e28
Tejas Pokalwar <pokalwartejas30102005@gmail.com>
2026-08-02 09:25:40 +0530

    Time: 593 ms (74.17%) | Memory: 184 MB (90.55%) - LeetSync

---
+++ 421-maximum-xor-of-two-numbers-in-an-array/maximum-xor-of-two-numbers-in-an-array.cpp
@@ -0,0 +1,67 @@
+class TrieNode{
+public:
+    TrieNode* child[2];
+
+    TrieNode(){
+        child[0]=child[1]=NULL;
+    }
+};
+
+class Solution {
+public:
+
+    TrieNode* root=new TrieNode();
+
+    void insert(int num){
+
+        TrieNode* node=root;
+
+        for(int i=31;i>=0;i--){
+
+            int bit=(num>>i)&1;
+
+            if(node->child[bit]==NULL)
+                node->child[bit]=new TrieNode();
+
+            node=node->child[bit];
+        }
+    }
+
+    int query(int num){
+
+        TrieNode* node=root;
+
+        int ans=0;
+
+        for(int i=31;i>=0;i--){
+
+            int bit=(num>>i)&1;
+
+            if(node->child[1-bit]){
+
+                ans|=(1<<i);
+
+                node=node->child[1-bit];
+            }
+            else{
+
+                node=node->child[bit];
+            }
+        }
+
+        return ans;
+    }
+
+    int findMaximumXOR(vector<int>& nums) {
+
+        for(int x:nums)
+            insert(x);
+
+        int ans=0;
+
+        for(int x:nums)
+            ans=max(ans,query(x));
+
+        return ans;
+    }
+};
