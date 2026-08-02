b63a5984a941f2ca64a665a5679a373924d9953e
Tejas Pokalwar <pokalwartejas30102005@gmail.com>
2026-08-02 10:07:24 +0530

    Time: 736 ms (80.92%) | Memory: 36.9 MB (86.19%) - LeetSync

---
+++ 4371-maximize-pair-strength-using-gcd/maximize-pair-strength-using-gcd.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    long long maxPairStrength(vector<int>& nums) {
+        long long ans = 0;
+        int n = nums.size();
+
+for(int i=0;i<n;i++){
+    for(int j=i+1;j<n;j++){
+
+        long long g = gcd(nums[i],nums[j]);
+
+        long long cur =
+            1LL*nums[i]/g * (nums[j]/g);
+
+        ans=max(ans,cur);
+    }
+}
+    return ans;
+    }
+};
